050256d4d82a1fa519c9cc04e1f359807e728bdb
Richard Westmoreland <rawestmoreland@gmail.com>
2026-03-28 12:34:12 +0100

    Add migration to update 'watches' collection fields
    - Removed the 'text' field and added a new 'editor' field for notes in the 'watches' collection.
    - Ensured the migration supports both applying and reverting changes to maintain data integrity.

---
+++ pocketbase/base/migrations/1774697633_updated_watches.go
@@ -0,0 +1,63 @@
+package migrations
+
+import (
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_1423682000")
+		if err != nil {
+			return err
+		}
+
+		// remove field
+		collection.Fields.RemoveById("text18589324")
+
+		// add field
+		if err := collection.Fields.AddMarshaledJSONAt(15, []byte(`{
+			"convertURLs": false,
+			"hidden": false,
+			"id": "editor18589324",
+			"maxSize": 0,
+			"name": "notes",
+			"presentable": false,
+			"required": false,
+			"system": false,
+			"type": "editor"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_1423682000")
+		if err != nil {
+			return err
+		}
+
+		// add field
+		if err := collection.Fields.AddMarshaledJSONAt(14, []byte(`{
+			"autogeneratePattern": "",
+			"hidden": false,
+			"id": "text18589324",
+			"max": 5000,
+			"min": 0,
+			"name": "notes",
+			"pattern": "",
+			"presentable": false,
+			"primaryKey": false,
+			"required": false,
+			"system": false,
+			"type": "text"
+		}`)); err != nil {
+			return err
+		}
+
+		// remove field
+		collection.Fields.RemoveById("editor18589324")
+
+		return app.Save(collection)
+	})
+}
