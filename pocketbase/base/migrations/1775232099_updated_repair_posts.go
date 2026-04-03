7e4bd8d61052fcdfe681bc184cb023970e98fdaa
Richard Westmoreland <rawestmoreland@gmail.com>
2026-04-03 18:01:56 +0200

    chore: auth for blog

---
+++ pocketbase/base/migrations/1775232099_updated_repair_posts.go
@@ -0,0 +1,44 @@
+package migrations
+
+import (
+	"encoding/json"
+
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_2132425296")
+		if err != nil {
+			return err
+		}
+
+		// update collection data
+		if err := json.Unmarshal([]byte(`{
+			"createRule": "@request.auth.id != \"\"",
+			"deleteRule": "@request.auth.id != \"\"",
+			"updateRule": "@request.auth.id != \"\""
+		}`), &collection); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_2132425296")
+		if err != nil {
+			return err
+		}
+
+		// update collection data
+		if err := json.Unmarshal([]byte(`{
+			"createRule": null,
+			"deleteRule": null,
+			"updateRule": null
+		}`), &collection); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	})
+}
