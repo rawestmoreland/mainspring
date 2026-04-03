8e3a8709d3659a8b8a2ac33fc7d117a141c0275e
Richard Westmoreland <rawestmoreland@gmail.com>
2026-04-03 18:59:37 +0200

    feat: add body_plain field to repair posts collection
    - Introduced a new field 'body_plain' to the 'pbc_2132425296' collection for enhanced data structure.
    - Implemented migration logic to add and remove the field as needed.

---
+++ pocketbase/base/migrations/1775235564_updated_repair_posts.go
@@ -0,0 +1,45 @@
+package migrations
+
+import (
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_2132425296")
+		if err != nil {
+			return err
+		}
+
+		// add field
+		if err := collection.Fields.AddMarshaledJSONAt(6, []byte(`{
+			"autogeneratePattern": "",
+			"hidden": false,
+			"id": "text3133793799",
+			"max": 0,
+			"min": 0,
+			"name": "body_plain",
+			"pattern": "",
+			"presentable": false,
+			"primaryKey": false,
+			"required": false,
+			"system": false,
+			"type": "text"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_2132425296")
+		if err != nil {
+			return err
+		}
+
+		// remove field
+		collection.Fields.RemoveById("text3133793799")
+
+		return app.Save(collection)
+	})
+}
