149a4fad5ab9383428084a2116b98e0d418641ca
Richard Westmoreland <rawestmoreland@gmail.com>
2026-05-04 09:15:30 +0200

    fix: update deploy script to remove --keep-vars option and add new migration for watch status field

---
+++ pocketbase/base/migrations/1777878913_updated_watches.go
@@ -0,0 +1,65 @@
+package migrations
+
+import (
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_1423682000")
+		if err != nil {
+			return err
+		}
+
+		// update field
+		if err := collection.Fields.AddMarshaledJSONAt(7, []byte(`{
+			"hidden": false,
+			"id": "select2063623452",
+			"maxSelect": 1,
+			"name": "status",
+			"presentable": false,
+			"required": true,
+			"system": false,
+			"type": "select",
+			"values": [
+				"acquired",
+				"in_progress",
+				"paused",
+				"listed",
+				"sold"
+			]
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_1423682000")
+		if err != nil {
+			return err
+		}
+
+		// update field
+		if err := collection.Fields.AddMarshaledJSONAt(7, []byte(`{
+			"hidden": false,
+			"id": "select2063623452",
+			"maxSelect": 1,
+			"name": "status",
+			"presentable": false,
+			"required": true,
+			"system": false,
+			"type": "select",
+			"values": [
+				"acquired",
+				"in_progress",
+				"listed",
+				"sold"
+			]
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	})
+}
