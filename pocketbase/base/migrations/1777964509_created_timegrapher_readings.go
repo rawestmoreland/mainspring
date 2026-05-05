84b95161c65796c5f65c86394c22e9a598cc0bae
Richard Westmoreland <rawestmoreland@gmail.com>
2026-05-05 09:02:18 +0200

    feat: add migration for timegrapher_readings collection with fields and rules

---
+++ pocketbase/base/migrations/1777964509_created_timegrapher_readings.go
@@ -0,0 +1,346 @@
+package migrations
+
+import (
+	"encoding/json"
+
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		jsonData := `{
+			"createRule": null,
+			"deleteRule": null,
+			"fields": [
+				{
+					"autogeneratePattern": "[a-z0-9]{15}",
+					"hidden": false,
+					"id": "text3208210256",
+					"max": 15,
+					"min": 15,
+					"name": "id",
+					"pattern": "^[a-z0-9]+$",
+					"presentable": false,
+					"primaryKey": true,
+					"required": true,
+					"system": true,
+					"type": "text"
+				},
+				{
+					"cascadeDelete": true,
+					"collectionId": "pbc_1423682000",
+					"hidden": false,
+					"id": "relation1342917158",
+					"maxSelect": 1,
+					"minSelect": 0,
+					"name": "watch",
+					"presentable": false,
+					"required": true,
+					"system": false,
+					"type": "relation"
+				},
+				{
+					"hidden": false,
+					"id": "select2063623452",
+					"maxSelect": 1,
+					"name": "status",
+					"presentable": false,
+					"required": true,
+					"system": false,
+					"type": "select",
+					"values": [
+						"before_service",
+						"after_service"
+					]
+				},
+				{
+					"hidden": false,
+					"id": "number3981045317",
+					"max": 70,
+					"min": 30,
+					"name": "lift_angle",
+					"onlyInt": false,
+					"presentable": false,
+					"required": true,
+					"system": false,
+					"type": "number"
+				},
+				{
+					"autogeneratePattern": "",
+					"hidden": false,
+					"id": "text18589324",
+					"max": 0,
+					"min": 0,
+					"name": "notes",
+					"pattern": "",
+					"presentable": false,
+					"primaryKey": false,
+					"required": false,
+					"system": false,
+					"type": "text"
+				},
+				{
+					"hidden": false,
+					"id": "number4254176197",
+					"max": null,
+					"min": null,
+					"name": "du_rate",
+					"onlyInt": false,
+					"presentable": false,
+					"required": false,
+					"system": false,
+					"type": "number"
+				},
+				{
+					"hidden": false,
+					"id": "number1236818277",
+					"max": null,
+					"min": null,
+					"name": "du_amp",
+					"onlyInt": false,
+					"presentable": false,
+					"required": false,
+					"system": false,
+					"type": "number"
+				},
+				{
+					"hidden": false,
+					"id": "number4219634497",
+					"max": null,
+					"min": null,
+					"name": "du_be",
+					"onlyInt": false,
+					"presentable": false,
+					"required": false,
+					"system": false,
+					"type": "number"
+				},
+				{
+					"hidden": false,
+					"id": "number890979067",
+					"max": null,
+					"min": null,
+					"name": "dd_rate",
+					"onlyInt": false,
+					"presentable": false,
+					"required": false,
+					"system": false,
+					"type": "number"
+				},
+				{
+					"hidden": false,
+					"id": "number339273559",
+					"max": null,
+					"min": null,
+					"name": "dd_amp",
+					"onlyInt": false,
+					"presentable": false,
+					"required": false,
+					"system": false,
+					"type": "number"
+				},
+				{
+					"hidden": false,
+					"id": "number321369019",
+					"max": null,
+					"min": null,
+					"name": "dd_be",
+					"onlyInt": false,
+					"presentable": false,
+					"required": false,
+					"system": false,
+					"type": "number"
+				},
+				{
+					"hidden": false,
+					"id": "number943107403",
+					"max": null,
+					"min": null,
+					"name": "cu_rate",
+					"onlyInt": false,
+					"presentable": false,
+					"required": false,
+					"system": false,
+					"type": "number"
+				},
+				{
+					"hidden": false,
+					"id": "number1421699549",
+					"max": null,
+					"min": null,
+					"name": "cu_amp",
+					"onlyInt": false,
+					"presentable": false,
+					"required": false,
+					"system": false,
+					"type": "number"
+				},
+				{
+					"hidden": false,
+					"id": "number1235378001",
+					"max": null,
+					"min": null,
+					"name": "cu_be",
+					"onlyInt": false,
+					"presentable": false,
+					"required": false,
+					"system": false,
+					"type": "number"
+				},
+				{
+					"hidden": false,
+					"id": "number4038884469",
+					"max": null,
+					"min": null,
+					"name": "cd_rate",
+					"onlyInt": false,
+					"presentable": false,
+					"required": false,
+					"system": false,
+					"type": "number"
+				},
+				{
+					"hidden": false,
+					"id": "number155047919",
+					"max": null,
+					"min": null,
+					"name": "cd_amp",
+					"onlyInt": false,
+					"presentable": false,
+					"required": false,
+					"system": false,
+					"type": "number"
+				},
+				{
+					"hidden": false,
+					"id": "number2701619115",
+					"max": null,
+					"min": null,
+					"name": "cd_be",
+					"onlyInt": false,
+					"presentable": false,
+					"required": false,
+					"system": false,
+					"type": "number"
+				},
+				{
+					"hidden": false,
+					"id": "number485490200",
+					"max": null,
+					"min": null,
+					"name": "cl_rate",
+					"onlyInt": false,
+					"presentable": false,
+					"required": false,
+					"system": false,
+					"type": "number"
+				},
+				{
+					"hidden": false,
+					"id": "number961387566",
+					"max": null,
+					"min": null,
+					"name": "cl_amp",
+					"onlyInt": false,
+					"presentable": false,
+					"required": false,
+					"system": false,
+					"type": "number"
+				},
+				{
+					"hidden": false,
+					"id": "number1689470788",
+					"max": null,
+					"min": null,
+					"name": "cl_be",
+					"onlyInt": false,
+					"presentable": false,
+					"required": false,
+					"system": false,
+					"type": "number"
+				},
+				{
+					"hidden": false,
+					"id": "number624139763",
+					"max": null,
+					"min": null,
+					"name": "cr_rate",
+					"onlyInt": false,
+					"presentable": false,
+					"required": false,
+					"system": false,
+					"type": "number"
+				},
+				{
+					"hidden": false,
+					"id": "number3869095373",
+					"max": null,
+					"min": null,
+					"name": "cr_amp",
+					"onlyInt": false,
+					"presentable": false,
+					"required": false,
+					"system": false,
+					"type": "number"
+				},
+				{
+					"hidden": false,
+					"id": "number3564464104",
+					"max": null,
+					"min": null,
+					"name": "cr_be",
+					"onlyInt": false,
+					"presentable": false,
+					"required": false,
+					"system": false,
+					"type": "number"
+				},
+				{
+					"hidden": false,
+					"id": "autodate2990389176",
+					"name": "created",
+					"onCreate": true,
+					"onUpdate": false,
+					"presentable": false,
+					"system": false,
+					"type": "autodate"
+				},
+				{
+					"hidden": false,
+					"id": "autodate3332085495",
+					"name": "updated",
+					"onCreate": true,
+					"onUpdate": true,
+					"presentable": false,
+					"system": false,
+					"type": "autodate"
+				}
+			],
+			"id": "pbc_1271644974",
+			"indexes": [
+				"CREATE INDEX ` + "`" + `idx_s3wRWUXY3Z` + "`" + ` ON ` + "`" + `timegrapher_readings` + "`" + ` (` + "`" + `watch` + "`" + `)"
+			],
+			"listRule": "",
+			"name": "timegrapher_readings",
+			"system": false,
+			"type": "base",
+			"updateRule": null,
+			"viewRule": ""
+		}`
+
+		collection := &core.Collection{}
+		if err := json.Unmarshal([]byte(jsonData), &collection); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_1271644974")
+		if err != nil {
+			return err
+		}
+
+		return app.Delete(collection)
+	})
+}
