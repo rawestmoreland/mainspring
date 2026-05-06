7a8fe50bd67b61eb7b416d7bc096943b696cb7f7
Richard Westmoreland <rawestmoreland@gmail.com>
2026-05-06 16:15:22 +0200

    feat: add user relations to collections

---
+++ pocketbase/base/migrations/1778059439_created_user_profiles.go
@@ -0,0 +1,141 @@
+package migrations
+
+import (
+	"encoding/json"
+
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		jsonData := `{
+			"createRule": null,
+			"deleteRule": null,
+			"fields": [
+				{
+					"autogeneratePattern": "[a-z0-9]{15}",
+					"hidden": false,
+					"id": "text3208210256",
+					"max": 15,
+					"min": 15,
+					"name": "id",
+					"pattern": "^[a-z0-9]+$",
+					"presentable": false,
+					"primaryKey": true,
+					"required": true,
+					"system": true,
+					"type": "text"
+				},
+				{
+					"cascadeDelete": true,
+					"collectionId": "_pb_users_auth_",
+					"hidden": false,
+					"id": "relation2375276105",
+					"maxSelect": 1,
+					"minSelect": 0,
+					"name": "user",
+					"presentable": false,
+					"required": true,
+					"system": false,
+					"type": "relation"
+				},
+				{
+					"autogeneratePattern": "",
+					"hidden": false,
+					"id": "text3252000302",
+					"max": 0,
+					"min": 0,
+					"name": "subdomain",
+					"pattern": "^[a-z0-9][a-z0-9-]{1,61}[a-z0-9]$",
+					"presentable": false,
+					"primaryKey": false,
+					"required": false,
+					"system": false,
+					"type": "text"
+				},
+				{
+					"autogeneratePattern": "",
+					"hidden": false,
+					"id": "text3578368839",
+					"max": 256,
+					"min": 2,
+					"name": "display_name",
+					"pattern": "",
+					"presentable": false,
+					"primaryKey": false,
+					"required": true,
+					"system": false,
+					"type": "text"
+				},
+				{
+					"autogeneratePattern": "",
+					"hidden": false,
+					"id": "text3709889147",
+					"max": 0,
+					"min": 0,
+					"name": "bio",
+					"pattern": "",
+					"presentable": false,
+					"primaryKey": false,
+					"required": false,
+					"system": false,
+					"type": "text"
+				},
+				{
+					"hidden": false,
+					"id": "bool1811784642",
+					"name": "is_public",
+					"presentable": false,
+					"required": false,
+					"system": false,
+					"type": "bool"
+				},
+				{
+					"hidden": false,
+					"id": "autodate2990389176",
+					"name": "created",
+					"onCreate": true,
+					"onUpdate": false,
+					"presentable": false,
+					"system": false,
+					"type": "autodate"
+				},
+				{
+					"hidden": false,
+					"id": "autodate3332085495",
+					"name": "updated",
+					"onCreate": true,
+					"onUpdate": true,
+					"presentable": false,
+					"system": false,
+					"type": "autodate"
+				}
+			],
+			"id": "pbc_2190040129",
+			"indexes": [
+				"CREATE UNIQUE INDEX ` + "`" + `idx_OSFlI2FE1h` + "`" + ` ON ` + "`" + `user_profiles` + "`" + ` (` + "`" + `subdomain` + "`" + `)"
+			],
+			"listRule": null,
+			"name": "user_profiles",
+			"system": false,
+			"type": "base",
+			"updateRule": null,
+			"viewRule": null
+		}`
+
+		collection := &core.Collection{}
+		if err := json.Unmarshal([]byte(jsonData), &collection); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_2190040129")
+		if err != nil {
+			return err
+		}
+
+		return app.Delete(collection)
+	})
+}
