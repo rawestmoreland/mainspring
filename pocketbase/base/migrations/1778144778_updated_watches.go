33c0455a58a01f99e5eb0ee12c986a6e2a2aca5d
Richard Westmoreland <rawestmoreland@gmail.com>
2026-05-07 12:21:46 +0200

    feat: add migration scripts for updated collections and user permissions

---
+++ pocketbase/base/migrations/1778144778_updated_watches.go
@@ -0,0 +1,44 @@
+package migrations
+
+import (
+	"encoding/json"
+
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_1423682000")
+		if err != nil {
+			return err
+		}
+
+		// update collection data
+		if err := json.Unmarshal([]byte(`{
+			"createRule": "@request.auth.id != \"\" && @request.auth.id = user.id",
+			"deleteRule": "@request.auth.id != \"\" && @request.auth.id = user.id",
+			"updateRule": "@request.auth.id != \"\" && @request.auth.id = user.id"
+		}`), &collection); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_1423682000")
+		if err != nil {
+			return err
+		}
+
+		// update collection data
+		if err := json.Unmarshal([]byte(`{
+			"createRule": "@request.auth.id != \"\"",
+			"deleteRule": null,
+			"updateRule": "@request.auth.id != \"\""
+		}`), &collection); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	})
+}
