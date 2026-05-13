93919d161c9c1617acf442008e521d9a11fb5cff
Richard Westmoreland <rawestmoreland@gmail.com>
2026-05-13 08:33:05 +0200

    feat: add Drawer and update Sheet component to use vaul library chore: update package.json and package-lock.json to include vaul dependency

---
+++ pocketbase/base/migrations/1778653363_updated_watches.go
@@ -0,0 +1,50 @@
+package migrations
+
+import (
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_1423682000")
+		if err != nil {
+			return err
+		}
+
+		// add field
+		if err := collection.Fields.AddMarshaledJSONAt(16, []byte(`{
+			"hidden": false,
+			"id": "file207818326",
+			"maxSelect": 1,
+			"maxSize": 0,
+			"mimeTypes": [
+				"image/jpeg",
+				"image/png",
+				"image/heic",
+				"image/webp"
+			],
+			"name": "feature_image",
+			"presentable": false,
+			"protected": false,
+			"required": false,
+			"system": false,
+			"thumbs": [],
+			"type": "file"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_1423682000")
+		if err != nil {
+			return err
+		}
+
+		// remove field
+		collection.Fields.RemoveById("file207818326")
+
+		return app.Save(collection)
+	})
+}
