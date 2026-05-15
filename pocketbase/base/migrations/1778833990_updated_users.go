84082fade3d23cedb4d6b3d71a35dd434b31f563
Richard Westmoreland <rawestmoreland@gmail.com>
2026-05-15 10:33:17 +0200

    feat: update user collection schema to replace 'date746802699' with 'trial_ends_at' field

---
+++ pocketbase/base/migrations/1778833990_updated_users.go
@@ -0,0 +1,42 @@
+package migrations
+
+import (
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("_pb_users_auth_")
+		if err != nil {
+			return err
+		}
+
+		// remove field
+		collection.Fields.RemoveById("date746802699")
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("_pb_users_auth_")
+		if err != nil {
+			return err
+		}
+
+		// add field
+		if err := collection.Fields.AddMarshaledJSONAt(13, []byte(`{
+			"hidden": false,
+			"id": "date746802699",
+			"max": "",
+			"min": "",
+			"name": "trial_ends_at",
+			"presentable": false,
+			"required": false,
+			"system": false,
+			"type": "date"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	})
+}
