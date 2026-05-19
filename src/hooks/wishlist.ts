fc92ac4aae970166a1357f725f7972cc5d4f2d74
Richard Westmoreland <rawestmoreland@gmail.com>
2026-05-19 09:28:25 +0200

    feat: enhance wishlist hooks to include user-specific data and tracking events

--- src/hooks/wishlist.ts
+++ src/hooks/wishlist.ts
@@ -1,18 +1,34 @@
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { WishlistApi } from '#/lib/api/wishlist';
 import type { CreateWishlistItem, WishlistItem } from '#/types';
+import { useAuth } from './auth';
+import { usePostHog } from '@posthog/react';
+import { useGoogleAnalytics } from 'tanstack-router-ga4';
 
 export const useWishlist = () => {
+  const { user } = useAuth();
   return useQuery({
-    queryKey: ['wishlist'],
+    queryKey: ['wishlist', { userId: user?.id }],
     queryFn: () => WishlistApi.getItems(),
   });
 };
 
 export const useCreateWishlistItem = () => {
   const queryClient = useQueryClient();
+  const posthog = usePostHog();
+  const ga4 = useGoogleAnalytics();
   return useMutation({
     mutationFn: (data: CreateWishlistItem) => WishlistApi.createItem(data),
+    onSuccess: () => {
+      posthog.capture('wishlist_item_added', {
+        category: 'Wishlist',
+        label: 'Added a new item to the wishlist',
+      });
+      ga4.event('wishlist_item_added', {
+        category: 'Wishlist',
+        label: 'Added a new item to the wishlist',
+      });
+    },
     onSettled: () => {
       queryClient.invalidateQueries({ queryKey: ['wishlist'] });
     },
